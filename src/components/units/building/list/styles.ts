d6b808fade6945b79c960f38c7c6943ddced439e
Calmer <ty.calmer96@gmail.com>
2025-10-17 15:50:56 +0900

    styles: 모달 스타일 수정

--- src/components/units/building/list/styles.ts
+++ src/components/units/building/list/styles.ts
@@ -5,3 +5,17 @@ export const Container = styled.article`
   justify-content: center;
   align-items: center;
 `;
+export const ModalInner = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  .top {
+    display: flex;
+    flex-direction: column;
+    gap: 0.5rem;
+  }
+  .buttonWrap {
+    display: flex;
+    gap: 0.5rem;
+  }
+`;
